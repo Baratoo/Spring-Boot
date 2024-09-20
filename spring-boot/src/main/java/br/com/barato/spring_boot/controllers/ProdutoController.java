e0004c40b8d03013b6856b8ff4b078b42cfd04b9
Baratoo <kaique_luiz1@hotmail.com>
2024-09-20 15:12:29 -0300

    CRUD Produto

--- spring-boot/src/main/java/br/com/barato/spring_boot/controllers/ProdutoController.java
+++ spring-boot/src/main/java/br/com/barato/spring_boot/controllers/ProdutoController.java
@@ -1,8 +1,21 @@
 package br.com.barato.spring_boot.controllers;
 
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +30,64 @@ public class ProdutoController {
 	@Autowired //Sempre coloca um objeto
 	private IProdutoRepository produtoRepository;
 
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
-		Produto prod = new Produto(nome);
-		
-		produtoRepository.save(prod);
-		
-		return prod;
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) { //@Valid valida o que foi definido em Produto
+		produtoRepository.save(produto);
+		return produto;
 	}
+	
+	/*
+	@PostMapping
+	public @ResponseBody Produto novoProduto(
+			@RequestParam String nome,
+			@RequestParam double preco,
+			@RequestParam double desconto) {
+		
+		Produto prod = new Produto(nome, preco, desconto);	
+		produtoRepository.save(prod);
+		return prod;
+	}*/
+	
+	@GetMapping
+	public Iterable<Produto> getProdutos(){
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Produto> getProdutoById(@PathVariable int id){
+		return produtoRepository.findById(id);
+	}
+	
+	@GetMapping(path = "/nome/{parteNome}")
+	public Iterable<Produto> getProdutoByNome(@PathVariable String parteNome){
+		return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+		//return produtoRepository.searchByNameLike(parteNome);
+	}
+	
+	@GetMapping(path = "/pagina/{numPagina}/{qtdePagina}")
+	public Iterable<Produto> getProdutosByPag(@PathVariable int numPagina, @PathVariable int qtdePagina){
+		if(qtdePagina >= 5) qtdePagina = 5;
+		Pageable page = PageRequest.of(numPagina, qtdePagina); 
+		return produtoRepository.findAll(page);
+	}
+	
+	/* Pode ser utilizado junto do metodo de novoProduto
+	@PutMapping //Utiliza put para atualizar produto/objeto inteiro
+	public Produto updateProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}*/
+	
+	@DeleteMapping(path = "/{id}")
+	public void deleteProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+	
+	
+	
+	
+	
+	
+
+	
 }
--- spring-boot/src/main/java/br/com/barato/spring_boot/model/repositories/IProdutoRepository.java
+++ spring-boot/src/main/java/br/com/barato/spring_boot/model/repositories/IProdutoRepository.java
@@ -1,10 +1,28 @@
 package br.com.barato.spring_boot.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.barato.spring_boot.model.entities.Produto;
 
-public interface IProdutoRepository extends CrudRepository<Produto, Integer>{
+public interface IProdutoRepository extends PagingAndSortingRepository<Produto, Integer>{
 	
+	//Spring entende o findBy, desde que esteja na sintaxe que ele usa
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String nome);
 	
+	/*
+	 Outros exemplos
+	 
+	 findByNomeIsContaining
+	 findByNomeContains
+	 
+	 findByNomeStartWith
+	 findByNomeEndsWith
+	 
+	 findByNomeNotContaining
+	 */
+	
+	@Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param ("nome") String nome);
 }
